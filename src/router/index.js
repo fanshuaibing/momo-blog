6af50b5c9118e22eec18549ef90c70f084e90695
fatfanfan <fatfan1020@gmail.com>
2019-03-02 22:42:06 +0800

    静态页面、路由配置完成

---
+++ src/router/index.js
@@ -0,0 +1,66 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import store from '../store/'
+
+
+Vue.use(Router)
+
+const router =  new Router({
+  routes: [
+    {
+      path: '/',
+      component: ()=> import('@/pages/Index/template.vue'),
+    },
+    {
+      path: '/register',
+      component: ()=> import('@/pages/Register/template.vue'),
+    },
+    {
+      path: '/login',
+      component: ()=> import('@/pages/Login/template.vue'),
+    },
+    {
+      path: '/detail/:blogId',
+      component: ()=> import('@/pages/Detail/template.vue'),
+    },
+    {
+      path: '/edit/:blogId',
+      component: ()=> import('@/pages/Edit/template.vue'),
+      meta:{ requiresAuth: true}
+    },
+    {
+      path: '/create',
+      component: ()=> import('@/pages/Create/template.vue'),
+      meta:{ requiresAuth: true}
+    },
+    {
+      path: '/user/:userId',
+      component: ()=> import('@/pages/User/template.vue'),
+    },
+    {
+      path: '/my',
+      component: ()=> import('@/pages/My/template.vue'),
+      meta:{ requiresAuth: true}
+    }
+  ]
+})
+
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    store.dispatch('checkLogin').then((isLogin)=> {
+      if (!store.getters.isLogin) {
+        next({
+          path: '/login',
+          query: {redirect: to.fullPath}
+        })
+      } else {
+        next()
+      }
+    })
+  } else {
+    next()
+  }
+})
+
+export default  router
