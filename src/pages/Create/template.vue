cc34a440e9027d206c10783f4b0dd2a140b79766
fatfanfan <fatfan1020@gmail.com>
2019-03-02 23:52:54 +0800

    创建博客、首页完成

--- src/pages/Create/template.vue
+++ src/pages/Create/template.vue
@@ -2,15 +2,17 @@
   <div id="edit">
     <h1>创建文章</h1>
     <h3>文章标题</h3>
-    <el-input></el-input>
+    <el-input v-model="title"></el-input>
     <p class="msg">限30个字</p>
     <h3>内容简介</h3>
-    <el-input type="textarea" rows=3></el-input>
-    <p class="msg">限30个字</p>
+    <el-input type="textarea" v-model="description" :autosize="{minRows:5, maxRows:8}"></el-input>
     <h3>文章内容</h3>
-    <el-input type="textarea" rows=20></el-input>
-    <p class="msg">限30个字</p>
-    <el-button >确定</el-button>
+    <el-input type="textarea" v-model="content" :autosize="{minRows:10, maxRows:30}"> </el-input>
+    <p class="switchWrapper">
+      <label>是否展示到首页</label>
+      <el-switch v-model="atIndex" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+    </p>
+    <el-button @click="onCreate">确定</el-button>
   </div>
 </template>
 
